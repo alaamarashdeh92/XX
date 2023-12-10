798c7657c97d8d81ea05fe70b9b90d1c706456f1
ALaa Marashdeh <74057038+alaamarashdeh92@users.noreply.github.com>
2023-12-10 11:29:58 +0300

    Add files via upload

---
+++ C.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+    int a=11;
+    cout << "You entered " << number<<endl ;
+    cout<<a;
+    cout<<&a;
+    return 0;
+
+
+
+
+
+
+
+
+        
+}
---
+++ sum + avg.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int x,y,sum;
+double avg;
+cout<<"Enter 2 dig";
+cin>>x>>y;
+sum=x+y;
+avg=sum/2.0;
+cout<<sum<<endl;
+cout<<avg<<endl;
+return 0;
+}
